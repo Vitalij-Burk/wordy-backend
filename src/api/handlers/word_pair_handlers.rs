c85b7ea18fc1a10304477b0f4ce0cd6dd1ae8563
Vitalij-Burk <burkasovvitalij@gmail.com>
2026-02-26 09:40:10 +0600

    Preparation for VDS

--- src/api/handlers/word_pair_handlers.rs
+++ src/api/handlers/word_pair_handlers.rs
@@ -5,10 +5,9 @@ use axum::{
 };
 
 use crate::{
-    AppState,
-    application::services::{
-        user_service::UserServiceError, word_pair_service::WordPairServiceError,
-    },
+    AppState, api::models::translate::TranslateDTO, application::services::{
+        translate_service::TranslateServiceError, user_service::UserServiceError, word_pair_service::WordPairServiceError
+    }
 };
 use crate::{
     api::{
@@ -17,6 +16,110 @@ use crate::{
     },
     domain::types::ID,
 };
+
+#[axum::debug_handler]
+pub async fn translate_and_add_word_pair_by_user_id(
+    State(state): State<AppState>,
+    Path(user_id): Path<ID>,
+    Json(dto): Json<TranslateDTO>
+) -> Result<Json<WordPairDTO>, HandlerError> {
+    let translated = state
+        .translate_service
+        .translate_text(&dto)
+        .await
+        .map_err(|error| match error {
+            TranslateServiceError::TranslatorError(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Translation failed")
+            }
+            TranslateServiceError::NotFoundLanguage(_) => {
+                (StatusCode::BAD_REQUEST, "Language not found")
+            }
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, "Unknown error"),
+        })?;
+
+    let create_word_pair_dto = CreateWordPairDTO {
+        target_text: translated.target_text,
+        source_text: translated.source_text,
+        target_language: translated.target_language,
+        source_language: translated.source_language
+    };
+
+    let res = state
+        .word_pair_service
+        .create(&user_id, &create_word_pair_dto)
+        .await
+        .map_err(|error| match error {
+            WordPairServiceError::Database(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+            }
+            WordPairServiceError::WordPairAlreadyExists(_) => {
+                (StatusCode::UNPROCESSABLE_ENTITY, "Word pair exists")
+            }
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, "Unknown error"),
+        })?;
+
+    let dto = WordPairDTO::from(res);
+
+    Ok(Json(dto))
+}
+
+#[axum::debug_handler]
+pub async fn translate_and_add_word_pair_by_user_key(
+    State(state): State<AppState>,
+    Path(key): Path<String>,
+    Json(dto): Json<TranslateDTO>,
+) -> Result<Json<WordPairDTO>, HandlerError> {
+    let translated = state
+        .translate_service
+        .translate_text(&dto)
+        .await
+        .map_err(|error| match error {
+            TranslateServiceError::TranslatorError(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Translation failed")
+            }
+            TranslateServiceError::NotFoundLanguage(_) => {
+                (StatusCode::BAD_REQUEST, "Language not found")
+            }
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, "Unknown error"),
+        })?;
+
+    let create_word_pair_dto = CreateWordPairDTO {
+        target_text: translated.target_text,
+        source_text: translated.source_text,
+        target_language: translated.target_language,
+        source_language: translated.source_language
+    };
+
+    let user = state
+        .user_service
+        .get_by_key(&key)
+        .await
+        .map_err(|error| match error {
+            UserServiceError::Database(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+            }
+            UserServiceError::NotFound(_) => (StatusCode::NOT_FOUND, "User not found"),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, "Unknown error"),
+        })?;
+
+    let res = state
+        .word_pair_service
+        .create(&user.id, &create_word_pair_dto)
+        .await
+        .map_err(|error| match error {
+            WordPairServiceError::Database(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+            }
+            WordPairServiceError::WordPairAlreadyExists(_) => {
+                (StatusCode::UNPROCESSABLE_ENTITY, "Word pair exists")
+            }
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, "Unknown error"),
+        })?;
+
+    let dto = WordPairDTO::from(res);
+
+    Ok(Json(dto))
+}
 
 #[axum::debug_handler]
 pub async fn add_word_pair_by_user_id(
